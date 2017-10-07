9e2d261cca2542dedd163d85fb8d150da9e05a1b
Rafael Beck <hello@rafaelbeck.com>
2017-10-07 11:57:03 +0200

    Add waveform

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap';
+import WaveSurfer from 'wavesurfer.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './global.css';
 
@@ -6,19 +7,38 @@ class App {
     constructor() {
       this.tracks = ['D01.mp4', 'KS02.m4a', 'A03.m4a', 'W05.m4a', 'W01.m4a', 'W00.m4a'],
       this.current;
-      this.output = [];
+      this.output = [],
+      this.wavesurfer;
 
+      this.createWaveform();
       this.next();
     }
 
+    createWaveform() {
+      this.wavesurfer = WaveSurfer.create({
+        container: '#waveform',
+        waveColor: 'black',
+        progressColor: '#007bff'
+      });
+    }
+
+    toogle() {
+      this.wavesurfer.playPause()
+    }
+
     next() {
+
       this.current = this.tracks.shift();
-      $('#audio_player')[0].src = `./tracks/${this.current}`;
+      this.wavesurfer.load(`./tracks/${this.current}`);
 
       this.output.push($('#userInput').val());
       $('#userInput').val('');
 
       console.log(this.output);
+    }
+
+    getAudioElement() {
+      return $('#audio_player')[0];
     }
 
     setPlaybackRate(value) {
