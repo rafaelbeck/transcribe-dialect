d2e8d96f931d30a3838c433772f96eede9d1ca72
Rafael Beck <hello@rafaelbeck.com>
2017-10-07 12:30:35 +0200

    Add custom controls and configurations   * Button for slower/faster playbackrate   * Button for play/pause   * Button for skip the current task   * Display duration   * Loop Audio when it's duration is longer than 10s   * Autoplay   * Deactive autocomplete, autocorrect, and spellcheck

--- src/index.js
+++ src/index.js
@@ -1,48 +1,109 @@
 import 'bootstrap';
 import WaveSurfer from 'wavesurfer.js';
+import moment from 'moment';
+import 'moment-duration-format';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './global.css';
+import './public/global.css';
+import './public/icons.css';
 
 class App {
     constructor() {
       this.tracks = ['D01.mp4', 'KS02.m4a', 'A03.m4a', 'W05.m4a', 'W01.m4a', 'W00.m4a'],
+      this.tracksVisited = [];
       this.current;
       this.output = [],
       this.wavesurfer;
+      this.total = this.tracks.length;
+
+      moment.locale('de-ch');
 
       this.createWaveform();
+      this.createTextfield();
       this.next();
+    }
+
+    createTextfield() {
+      setInterval(() => {
+       $('#userInput').focus();
+       $('#userInput')[0].style.cssText = 'height: auto;';
+       $('#userInput')[0].style.cssText = 'height:' + ($('#userInput')[0].scrollHeight + 12) + 'px';
+
+       if (this.wavesurfer.isPlaying()) {
+         $('#buttonPause').removeClass('hidden');
+         $('#buttonPlay').addClass('hidden');
+       } else {
+         $('#buttonPlay').removeClass('hidden');
+         $('#buttonPause').addClass('hidden');
+       }
+
+       if (this.wavesurfer.getMute()) {
+         $('#buttonMute').removeClass('hidden');
+         $('#buttonUnmute').addClass('hidden');
+       } else {
+         $('#buttonUnmute').removeClass('hidden');
+         $('#buttonMute').addClass('hidden');
+       }
+     });
     }
 
     createWaveform() {
       this.wavesurfer = WaveSurfer.create({
         container: '#waveform',
-        waveColor: 'black',
-        progressColor: '#007bff'
+        waveColor: '#868e96',
+        progressColor: '#007bff',
+        barWidth: 0,
+        barHeight: 1
+      });
+      this.wavesurfer.on('ready', () => {
+        $('#duration').html(`Dauer: ${ moment.duration(this.wavesurfer.getDuration(), "milliseconds").format("s:SS")} Sekunden`);
+      });
+      this.wavesurfer.on('finish', () => {
+        if (this.wavesurfer.getDuration() < 10) { return; }
+        this.wavesurfer.playPause();
       });
     }
 
     toogle() {
-      this.wavesurfer.playPause()
+      this.wavesurfer.playPause();
+    }
+
+    mute(value) {
+      this.wavesurfer.setMute(value);
     }
 
     next() {
-
-      this.current = this.tracks.shift();
-      this.wavesurfer.load(`./tracks/${this.current}`);
-
       this.output.push($('#userInput').val());
-      $('#userInput').val('');
-
       console.log(this.output);
+      this.reset();
+      this.loadAudio();
     }
 
-    getAudioElement() {
-      return $('#audio_player')[0];
+    reset() {
+      $('#userInput').val('');
+    }
+
+    loadAudio() {
+      if (this.tracks.length === 0) {
+        this.tracks = this.tracksVisited;
+        this.tracksVisited = [];
+      }
+      this.current = this.tracks.shift();
+      this.tracksVisited.push(this.current);
+      this.wavesurfer.load(`./tracks/${this.current}`);
     }
 
     setPlaybackRate(value) {
-      $('#audio_player')[0].playbackRate = value;
+      this.wavesurfer.setPlaybackRate(this.wavesurfer.getPlaybackRate()+value);
+      if (this.wavesurfer.getPlaybackRate() > 1) {
+        $('#buttonFaster').addClass('active');
+        $('#buttonSlower').removeClass('active');
+      } else if (this.wavesurfer.getPlaybackRate() < 1) {
+        $('#buttonSlower').addClass('active');
+        $('#buttonFaster').removeClass('active');
+      } else {
+        $('#buttonSlower').removeClass('active');
+        $('#buttonFaster').removeClass('active');
+      }
     }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path    = require('path'),
       webpack = require('webpack'),
-      CopyWebpackPlugin = require('copy-webpack-plugin');
+      CopyWebpackPlugin = require('copy-webpack-plugin'),
+      autoprefixer = require('autoprefixer');
 
 module.exports = {
   entry: {
@@ -11,28 +12,32 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
   },
+
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(m4a|mp4)$/,
-        loader: 'file-loader'
-      }
-    ],
-    loaders: [
-       {
-         test: /\.js$/,
-         loader: 'babel-loader',
-         query: {
-          presets: ['es2015']
-         }
-       }
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+              presets: ["babel-preset-es2015"].map(require.resolve)
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+              'style-loader',
+              'css-loader',
+          ]
+        },
+        {
+            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: 'file-loader'
+        },
+        {
+            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: 'url-loader?limit=10000&minetype=application/font-woff'
+        }
      ]
   },
   plugins: [
