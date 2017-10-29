8b2bbbde1d51d9ea1c48bee657a3ce8b108c1261
Rafael Beck <hello@rafaelbeck.com>
2017-10-29 12:22:56 +0100

    Add custom controls and configurations   * Labels in dialect   * Show current/remaining time instead of total time   * Jump back 1 sec on the timeline when toogle play   * Keyboard shortcuts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap';
 import WaveSurfer from 'wavesurfer.js';
+import HumanInput from 'humaninput/dist/humaninput-full.min';
 import moment from 'moment';
 import 'moment-duration-format';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -18,15 +19,26 @@ class App {
       moment.locale('de-ch');
 
       this.createWaveform();
+      this.createHumanInput();
       this.createTextfield();
       this.next();
     }
 
+    createHumanInput() {
+      let HI = new HumanInput(window);
+      HI.filter = (e) => { return true }
+      HI.on('shift-space', (event) => { this.toggle();event.preventDefault(); });
+      HI.on('shift-tab', (event) => { this.setPlaybackRate(0.75);});
+      HI.on('shift-left', (event) => { this.wavesurfer.skipBackward(0.5); });
+      HI.on('shift-right', (event) => { this.wavesurfer.skipForward(0.5); });
+      HI.once('enter', (event) => { this.finish() });
+    }
+
     createTextfield() {
       setInterval(() => {
-       $('#userInput').focus();
-       $('#userInput')[0].style.cssText = 'height: auto;';
-       $('#userInput')[0].style.cssText = 'height:' + ($('#userInput')[0].scrollHeight + 12) + 'px';
+       $('#user-input').focus();
+       $('#user-input')[0].style.cssText = 'height: auto;';
+       $('#user-input')[0].style.cssText = 'height:' + ($('#user-input')[0].scrollHeight + 12) + 'px';
 
        if (this.wavesurfer.isPlaying()) {
          $('#buttonPause').removeClass('hidden');
@@ -54,32 +66,59 @@ class App {
         barWidth: 0,
         barHeight: 1
       });
-      this.wavesurfer.on('ready', () => {
-        $('#duration').html(`Dauer: ${ moment.duration(this.wavesurfer.getDuration(), "milliseconds").format("s:SS")} Sekunden`);
+      this.wavesurfer.on('audioprocess', () => {
+        this.updateTime();
+      });
+      this.wavesurfer.on('seek', () => {
+        this.updateTime();
       });
       this.wavesurfer.on('finish', () => {
         if (this.wavesurfer.getDuration() < 10) { return; }
         this.wavesurfer.playPause();
       });
+      this.updateTime();
     }
 
-    toogle() {
-      this.wavesurfer.playPause();
+    updateTime() {
+      let totalTime = this.wavesurfer.getDuration(),
+          currentTime = this.wavesurfer.getCurrentTime(),
+          remainingTime = totalTime - currentTime;
+      $('#current').html(`${ moment.duration(currentTime, "milliseconds").format("s:SS")} Sekunden`);
+      $('#remaining').html(`- ${ moment.duration(remainingTime, "milliseconds").format("s:SS")} Sekunden`);
+    }
+
+    toggle() {
+      let currentTime = this.wavesurfer.getCurrentTime(),
+          newTime = currentTime;
+      if (!this.wavesurfer.isPlaying()) {
+        if (currentTime >= 1) {
+          newTime = currentTime - 1;
+        } else {
+          newTime = 0;
+        }
+        this.wavesurfer.play(newTime);
+      } else {
+        this.wavesurfer.playPause();
+      }
     }
 
     mute(value) {
       this.wavesurfer.setMute(value);
     }
 
+    finish() {
+      $('#btn-continue').css('display', 'block');
+    }
+
     next() {
-      this.output.push($('#userInput').val());
+      this.output.push($('#user-input').val());
       console.log(this.output);
       this.reset();
       this.loadAudio();
     }
 
     reset() {
-      $('#userInput').val('');
+      $('#user-input').val('');
     }
 
     loadAudio() {
